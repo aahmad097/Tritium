3376a5adbcc5c1598a4ada47ad658b5937320584
S4R1N <37748671+S4R1N@users.noreply.github.com>
2020-06-09 22:35:23 -0400

    Update Tritium.go

--- Tritium.go
+++ Tritium.go
@@ -1,3 +1,27 @@
+/*
+
+  Copyright (C) 2020 Ali Ahmad
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; version 2
+  of the License only.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to
+  Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+  Contact Information:
+ 
+  Ali S. Ahmad (s4r1n97@gmail.com)
+
+*/
+
 package main
 
 import (
